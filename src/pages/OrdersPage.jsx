158c8649437e89fae59c94f92d492009092be4fa
TiagoLobo-source <65892221+TiagoLobo-source@users.noreply.github.com>
2023-10-28 01:17:27 +0100

    still styling

--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -144,10 +144,10 @@ function OrderPage() {
           <p>Date: {formatDate(order.orders[0].date)}</p>
           <p>Time: {formatTime(order.orders[0].date)}</p>
           {order.orders[0]?.products.map((product) => (
-            <div key={product.products._id}>
+            <div key={product.products[0].product}>
               <h3>Product Details</h3>
-              <p>Product Name: {product.products[0].name}</p>
-              <p>Product Price: ${product.products[0].price}</p>
+              <p>Product Name: {product.product.name}</p>
+              <p>Product Price: ${product.product.price}</p>
             </div>
           ))}
           <button onClick={() => openTrackingModal(order)}>
